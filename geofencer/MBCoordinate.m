994778031b7cff5bef875f7c10548f3f73587124
Moshe Berman <moshberm@gmail.com>
2012-09-14 15:52:34 -0400

    Converted code to modern Objective-C and bracket syntax.

--- geofencer/MBCoordinate.m
+++ geofencer/MBCoordinate.m
@@ -11,28 +11,25 @@
 
 @implementation MBCoordinate
 
-@synthesize latitude = _latitude, longitude = _longitude;
-@synthesize isDragging;
-
 - (id) initWithLatitude:(double)latitude andLongitude:(double)longitude{
     self = [super init];
     
     if (self) {
-        self.latitude = latitude;
-        self.longitude = longitude;
-        self.isDragging = NO;
+        _latitude = latitude;
+        _longitude = longitude;
+        _isDragging = NO;
     }
     
     return self;
 }
 
 - (CLLocationCoordinate2D)CLLocationCoordinate2DRepresentation{
-    return CLLocationCoordinate2DMake(self.latitude, self.longitude);
+    return CLLocationCoordinate2DMake([self latitude], [self longitude]);
 }
 
 - (BOOL)isEqual:(id)object{
     if ([object respondsToSelector:@selector(latitude)] && [object respondsToSelector:@selector(longitude)]) {
-        return [object latitude] == self.latitude && [object longitude] == self.longitude;
+        return [object latitude] == [self latitude] && [object longitude] == [self longitude];
     }
     
     return NO;
@@ -44,8 +41,8 @@
 
 - (NSDictionary *)asDictionary{
     NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] init];
-    dictionary[@"latitude"] = @(self.latitude);
-    dictionary[@"longitude"] = @(self.longitude);
+    dictionary[@"latitude"] = @([self latitude]);
+    dictionary[@"longitude"] = @([self longitude]);
     return dictionary;
 }
 
