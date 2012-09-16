9dc2e9924dfb9e39037bbbcfa48a1a515401b4c8
Moshe Berman <moshberm@gmail.com>
2012-09-15 23:17:41 -0400

    Created data model object.

---
+++ geofencer/MBGeofenceCollection.h
@@ -0,0 +1,13 @@
+//
+//  MBGeofenceCollection.h
+//  Fence
+//
+//  Created by Moshe Berman on 9/15/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBGeofenceCollection : NSObject
+
+@end
---
+++ geofencer/MBGeofenceCollection.m
@@ -0,0 +1,13 @@
+//
+//  MBGeofenceCollection.m
+//  Fence
+//
+//  Created by Moshe Berman on 9/15/12.
+//
+//
+
+#import "MBGeofenceCollection.h"
+
+@implementation MBGeofenceCollection
+
+@end
