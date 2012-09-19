c1bdf2484106a5558352f2207945458f3cd9c95b
Moshe Berman <moshberm@gmail.com>
2012-09-19 18:46:36 -0400

    Removed newline before curly. Egyptian style!

--- geofencer/MBGeofenceCollection.m
+++ geofencer/MBGeofenceCollection.m
@@ -114,8 +114,7 @@
 //
 
 
-int pnpoly(int nvert, double *vertx, double *verty, double testx, double testy)
-{
+int pnpoly(int nvert, double *vertx, double *verty, double testx, double testy) {
     int i, j, c = 0;
     for (i = 0, j = nvert-1; i < nvert; j = i++) {
         if ( ((verty[i]>testy) != (verty[j]>testy)) &&
