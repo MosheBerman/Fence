7d7a96187f42ab77a06a9e063144630676aaceb1
Moshe Berman <moshberm@gmail.com>
2012-09-20 23:42:18 -0400

    Added Welcome VC

---
+++ geofencer/MBWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBWelcomeViewController.h
+//  Fence
+//
+//  Created by Moshe Berman on 9/20/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBWelcomeViewController : UIViewController
+
+@end
---
+++ geofencer/MBWelcomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  MBWelcomeViewController.m
+//  Fence
+//
+//  Created by Moshe Berman on 9/20/12.
+//
+//
+
+#import "MBWelcomeViewController.h"
+
+@interface MBWelcomeViewController ()
+
+@end
+
+@implementation MBWelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
