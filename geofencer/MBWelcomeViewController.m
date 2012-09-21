0971638736b8bf39808f58fc14fbbaf0c421992b
Moshe Berman <moshberm@gmail.com>
2012-09-20 23:53:43 -0400

    Made 2 nibs for welcome view.

--- geofencer/MBWelcomeViewController.m
+++ geofencer/MBWelcomeViewController.m
@@ -16,6 +16,7 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
+    
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -27,6 +28,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
