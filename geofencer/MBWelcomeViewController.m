12c6cbc8ca0a13bb88b4db28d7327f63d861f64b
Moshe Berman <moshberm@gmail.com>
2012-09-21 01:50:19 -0400

    Wired up welcome screen.

--- geofencer/MBWelcomeViewController.m
+++ geofencer/MBWelcomeViewController.m
@@ -8,7 +8,13 @@
 
 #import "MBWelcomeViewController.h"
 
-@interface MBWelcomeViewController ()
+#import <QuartzCore/QuartzCore.h>
+
+@interface MBWelcomeViewController () <UIScrollViewDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *headerImage;
+@property (strong, nonatomic) IBOutlet UIScrollView *wrapperScrollView;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UITextView *instructionsView;
 
 @end
 
@@ -36,6 +42,20 @@
     UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(next)];
     [[self navigationItem] setRightBarButtonItem:buttonItem animated:YES];
     
+    [[[self headerImage] layer] setShadowRadius:7];
+    [[[self headerImage] layer] setShadowColor:[UIColor blackColor].CGColor];
+    [[[self headerImage] layer] setShadowOpacity:1.0];
+    
+    [self setTitle:NSLocalizedString(@"Welcome", @"The title for the welcome screen")];
+    
+    CGFloat height = [[self headerImage] frame].size.height + [[self titleLabel] frame].size.height+[[self instructionsView] frame].size.height;
+    
+    CGSize newContentSize = CGSizeMake([[self wrapperScrollView]frame].size.width, height);
+    
+    [[self wrapperScrollView] setContentSize:newContentSize];
+    [[self wrapperScrollView] setShowsHorizontalScrollIndicator:NO];
+    
+    [[self instructionsView] setUserInteractionEnabled:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,5 +67,24 @@
 
 - (void) next{
     [[[self navigationController] presentingViewController] dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+
+    CGRect headerFrame = [[self headerImage] frame];
+    CGPoint contentOffset = [[self wrapperScrollView] contentOffset];
+    
+    headerFrame.origin.y = MIN(contentOffset.y, contentOffset.y - (contentOffset.y * 0.05));
+
+    [[self headerImage] setFrame:headerFrame];
+    
+}
+
+- (void)viewDidUnload {
+    [self setHeaderImage:nil];
+    [self setWrapperScrollView:nil];
+    [self setTitleLabel:nil];
+    [self setInstructionsView:nil];
+    [super viewDidUnload];
 }
 @end
